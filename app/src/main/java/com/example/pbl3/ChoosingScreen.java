5b9ae5c968bf987cd4009676b79746d047f71e09
PacliKilla <64590513+PacliKilla@users.noreply.github.com>
2020-11-25 17:15:38 +0200

    Chosing screen v.1

--- app/src/main/java/com/example/pbl3/ChoosingScreen.java
+++ app/src/main/java/com/example/pbl3/ChoosingScreen.java
@@ -8,7 +8,12 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
-
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 public class ChoosingScreen extends Fragment {
 
     @Override
@@ -31,4 +36,5 @@ public class ChoosingScreen extends Fragment {
             }
         });
     }
+
 }
--- app/src/main/java/com/example/pbl3/Display.java
+++ app/src/main/java/com/example/pbl3/Display.java
@@ -1,4 +1,16 @@
 package com.example.pbl3;
 
-public class Display {
-}
+import android.graphics.Color;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.Toast;
+
+
