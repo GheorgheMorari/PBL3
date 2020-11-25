fc82e3c4164dbc6966c872616e5804ae785c3730
Gheorghe Morari <gmorari404@gmail.com>
2020-11-25 19:13:06 +0200

    Fixed bugs, next time press ctrl+s and check if the shit works

--- app/src/main/java/com/example/pbl3/EndScreen.java
+++ app/src/main/java/com/example/pbl3/EndScreen.java
@@ -23,7 +23,7 @@ public class EndScreen extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_second).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.btn_option_two).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(EndScreen.this)
