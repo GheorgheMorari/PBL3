9569e00df09d093a01a1fcd80caf7b2d67ae8c0a
PacliKilla <64590513+PacliKilla@users.noreply.github.com>
2020-12-08 21:47:24 +0200

    A few optimisations( plz helmp )

--- app/src/main/java/com/example/pbl3/ChoosingScreen.java
+++ app/src/main/java/com/example/pbl3/ChoosingScreen.java
@@ -14,9 +14,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static com.example.pbl3.DisplayInteraction.guess;
+import static com.example.pbl3.DisplayInteraction.questionSet;
+import static com.example.pbl3.QuestionSet.questions;
+
 
 public class ChoosingScreen extends Fragment {
 
@@ -26,31 +30,46 @@ public class ChoosingScreen extends Fragment {
     //TODO pass the value parameter to the selectOption() as DisplayInteraction.agree
     //TODO IMPLEMENT SWITCHING TO ENDSCREEN WHEN guess.confidence >= 0.80f;
     Question nextQuestion;
+
+    private TextView mTextView;
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
+
     ) {
+        mTextView = container.findViewById(R.id.choosing_screen_question);
+        ShowText();
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.choosing_screen, container, false);
     }
 
+    public void ShowText(){
+        mTextView.setText( DisplayInteraction.getNextQuestion().visibleString);
+    }
+
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        //nextQuestion = DisplayInteraction.getNextQuestion();
+
+
+        nextQuestion = DisplayInteraction.getNextQuestion();
+
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.btn_option_two).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.btn_agree
+        ).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               // DisplayInteraction.selectOption(nextQuestion,DisplayInteraction.agree);
+                ShowText();
+               DisplayInteraction.selectOption(nextQuestion,DisplayInteraction.agree);
 
-               // nextQuestion = DisplayInteraction.getNextQuestion();
+               nextQuestion = DisplayInteraction.getNextQuestion();
+
+                if (guess.confidence >= 0.80f)
+                    NavHostFragment.findNavController(ChoosingScreen.this).navigate(R.id.action_SecondFragment_to_ThirdFragment);
 
 
-
-                   NavHostFragment.findNavController(ChoosingScreen.this).navigate(R.id.action_SecondFragment_to_ThirdFragment);
-               if (guess.confidence >= 0.80f) {
-              }
             }
         });
     }
